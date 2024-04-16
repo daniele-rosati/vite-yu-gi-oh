a47f44925205283e1ae0d7504685b935d1fede0d
daniele-rosati <drosati151@gmail.com>
2024-04-16 22:54:52 +0200

    main js + header + card

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from 'bootstrap' //per boostrap
 import App from './App.vue'
 
 createApp(App).mount('#app')
