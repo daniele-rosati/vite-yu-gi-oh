25137380571d8008ccf5eba69deeb48b79b8e9e3
daniele-rosati <drosati151@gmail.com>
2024-04-16 19:18:41 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import axios from 'axios';
+import { store } from './store.js';
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  components: {
+    AppHeader
+  },
+  data() {
+    return {
+      store
+    };
+  }
+}
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+</template>
+
+<style lang="scss" scoped>
+      @use './style/general.scss';
+</style>
